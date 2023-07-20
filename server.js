55b004abab189354987f8d4df39457cfa2551976
sho96 <shotaro962008@gmail.com>
2023-07-20 16:37:20 +0900

    bug fix

--- server.js
+++ server.js
@@ -315,6 +315,11 @@ app.post("/apps/rate/submitRating", (req, resp) => {
 app.get("/getDashboard", (req, resp) => {
   const username = req.query.username;
   const travelDatas = readFileSync("./jsons/travelDatas.json", {encoding: "utf-8"});
+  if(!(username in travelDatas)){
+    resp.setHeader("Content-Type", "application/json");
+    resp.end(JSON.stringify({totalDistanceTraveled:0, totalSuddenBrakes:0, totalRearImpacts:0}));
+    return;
+  }
   let data = {};
   data["totalDistanceTraveled"] = travelDatas.username.totalDistanceTraveled;
   delete travelDatas;
