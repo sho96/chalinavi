2dd33f12273e2e22ae1a0d2dbcc7a8cfbb670b15
sho96 <shotaro962008@gmail.com>
2023-07-25 00:24:17 +0900

    update

--- server.js
+++ server.js
@@ -699,41 +699,78 @@ app.get("/imgs/apps/rate/rate.png", (req, resp) => {
 });
 
 //----------------------------- responding to sound requests -------------------------------
-app.get("/sounds/10m.mp3", (req, resp) => {
-  resp.status(200).send(readFileSync("./sounds/10m.mp3"));
+app.get("/sounds/ja/10m.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/ja/10m.mp3"));
 });
-app.get("/sounds/20m.mp3", (req, resp) => {
-  resp.status(200).send(readFileSync("./sounds/20m.mp3"));
+app.get("/sounds/ja/20m.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/ja/20m.mp3"));
 });
-app.get("/sounds/30m.mp3", (req, resp) => {
-  resp.status(200).send(readFileSync("./sounds/30m.mp3"));
+app.get("/sounds/ja/30m.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/ja/30m.mp3"));
 });
-app.get("/sounds/40m.mp3", (req, resp) => {
-  resp.status(200).send(readFileSync("./sounds/40m.mp3"));
+app.get("/sounds/ja/40m.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/ja/40m.mp3"));
 });
-app.get("/sounds/50m.mp3", (req, resp) => {
-  resp.status(200).send(readFileSync("./sounds/50m.mp3"));
+app.get("/sounds/ja/50m.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/ja/50m.mp3"));
 });
-app.get("/sounds/60m.mp3", (req, resp) => {
-  resp.status(200).send(readFileSync("./sounds/60m.mp3"));
+app.get("/sounds/ja/60m.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/ja/60m.mp3"));
 });
-app.get("/sounds/70m.mp3", (req, resp) => {
-  resp.status(200).send(readFileSync("./sounds/70m.mp3"));
+app.get("/sounds/ja/70m.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/ja/70m.mp3"));
 });
-app.get("/sounds/80m.mp3", (req, resp) => {
-  resp.status(200).send(readFileSync("./sounds/80m.mp3"));
+app.get("/sounds/ja/80m.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/ja/80m.mp3"));
 });
-app.get("/sounds/90m.mp3", (req, resp) => {
-  resp.status(200).send(readFileSync("./sounds/90m.mp3"));
+app.get("/sounds/ja/90m.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/ja/90m.mp3"));
 });
-app.get("/sounds/100m.mp3", (req, resp) => {
-  resp.status(200).send(readFileSync("./sounds/100m.mp3"));
+app.get("/sounds/ja/100m.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/ja/100m.mp3"));
 });
-app.get("/sounds/rear_impact.mp3", (req, resp) => {
-  resp.status(200).send(readFileSync("./sounds/rear_impact.mp3"));
+app.get("/sounds/ja/rear_impact.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/ja/rear_impact.mp3"));
 });
-app.get("/sounds/sudden_brake.mp3", (req, resp) => {
-  resp.status(200).send(readFileSync("./sounds/sudden_brake.mp3"));
+app.get("/sounds/ja/sudden_brake.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/ja/sudden_brake.mp3"));
+});
+
+app.get("/sounds/en/10m.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/en/10m.mp3"));
+});
+app.get("/sounds/en/20m.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/en/20m.mp3"));
+});
+app.get("/sounds/en/30m.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/en/30m.mp3"));
+});
+app.get("/sounds/en/40m.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/en/40m.mp3"));
+});
+app.get("/sounds/en/50m.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/en/50m.mp3"));
+});
+app.get("/sounds/en/60m.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/en/60m.mp3"));
+});
+app.get("/sounds/en/70m.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/en/70m.mp3"));
+});
+app.get("/sounds/en/80m.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/en/80m.mp3"));
+});
+app.get("/sounds/en/90m.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/en/90m.mp3"));
+});
+app.get("/sounds/en/100m.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/en/100m.mp3"));
+});
+app.get("/sounds/en/rear_impact.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/en/rear_impact.mp3"));
+});
+app.get("/sounds/en/sudden_brake.mp3", (req, resp) => {
+  resp.status(200).send(readFileSync("./sounds/en/sudden_brake.mp3"));
 });
 
 //----------------------------- responding to json requests -------------------------------
