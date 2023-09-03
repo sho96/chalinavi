e6ad8cd0a0e1696c17d9ad322f8e42ca10bbb158
sho96 <shotaro962008@gmail.com>
2023-09-03 14:06:52 +0900

    update

--- sendRequest.py
+++ sendRequest.py
@@ -4,6 +4,6 @@ import time
 interval = 60 * 13
 
 while True:
-    resp = req.get("https://chalinavi.onrender.com/login")
+    resp = req.get("https://mamo-ru.onrender.com/login")
     print(f"server responded with a status code of {resp.status_code}")
     time.sleep(interval)
